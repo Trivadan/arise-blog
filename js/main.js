170fb33f89d9c73bf32b9166a204e1b5087edd5f
Danstan <danstanochieng@gmail.com>
2017-07-02 19:06:37 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+    $(".filter-button").click(function(){
+        var value = $(this).attr('data-filter');
+        
+        if(value == "all")
+        {
+            //$('.filter').removeClass('hidden');
+            $('.grid-boxes-in').show('1000');
+        }
+        else
+        {
+//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+            $(".grid-boxes-in").not('.'+value).hide('3000');
+            $('.grid-boxes-in').filter('.'+value).show('3000');
+            
+        }
+    });
+    
+    if ($(".filter-button").removeClass("active")) {
+$(this).removeClass("active");
+}
+$(this).addClass("active");
+
+});
